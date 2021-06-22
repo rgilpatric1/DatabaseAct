23f79a912022d07d3485d8af520c541840c6ef44
Benjamin Kim <22kimb@elmbrookstudenst.org>
2021-06-22 14:54:07 -0500

    Work1

---
+++ CodeTheWay/CodeTheWay.Web.Ui/Repositories/IShippingContainerRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace CodeTheWay.Web.Ui.Repositories
+{
+    public interface IShippingContainerRepository
+    {
+    }
+}
