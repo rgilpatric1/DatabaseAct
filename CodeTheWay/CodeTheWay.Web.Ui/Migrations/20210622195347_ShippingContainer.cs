effe25921bdbce76dfed31f15be70d741f772bdc
SumanthKota <sumanthkota684@gmail.com>
2021-06-22 14:55:17 -0500

    Updated Database

---
+++ CodeTheWay/CodeTheWay.Web.Ui/Migrations/20210622195347_ShippingContainer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CodeTheWay.Web.Ui.Migrations
+{
+    public partial class ShippingContainer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Students",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "ShippingContainer",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Weight = table.Column<int>(nullable: false),
+                    Destination = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShippingContainer", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShippingContainer");
+
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Students");
+        }
+    }
+}
